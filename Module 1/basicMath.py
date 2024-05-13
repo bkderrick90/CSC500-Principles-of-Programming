680da269443170a2129bf0daf1e019e367e8bd97
bkderrick90 <48165176+bkderrick90@users.noreply.github.com>
2024-05-13 15:57:55 -0500

    Create basicMath.py

---
+++ Module 1/basicMath.py
@@ -0,0 +1,5 @@
+num1 = input("First number: ")
+num2 = input("Second number: ")
+
+print("The sum of ", num1, " and ", num2, " is: ", num1 + num2)
+print("The difference of ", num1, " and ", num2, " is: ", num1 - num2)

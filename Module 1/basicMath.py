d297432f9a65a39640f6d22b568d0a7518ea4e9e
bkderrick90 <48165176+bkderrick90@users.noreply.github.com>
2024-05-13 16:21:08 -0500

    Update basicMath.py

--- Module 1/basicMath.py
+++ Module 1/basicMath.py
@@ -1,5 +1,8 @@
 num1 = input("First number: ")
 num2 = input("Second number: ")
 
-print("The sum of ", num1, " and ", num2, " is: ", num1 + num2)
-print("The difference of ", num1, " and ", num2, " is: ", num1 - num2)
+num1 = int(num1)
+num2 = int(num2)
+
+print("The sum of", num1, "and", num2, "is:", num1 + num2)
+print("The difference between", num1, "and", num2, "is:", num1 - num2)

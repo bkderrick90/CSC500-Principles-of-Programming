cca212f9411da383c8409a9c3f426631a1a5d07b
Brennan Derrick <bkderrick90@gmail.com>
2024-06-11 14:37:04 -0500

    Module 3 Completion

--- Module 5/module5.py
+++ Module 5/module5.py
@@ -4,59 +4,59 @@
 # inches of rainfall for that month. After all iterations, the program should display the number of months, the total
 # inches of rainfall, and the average rainfall per month for the entire period.
 
-print("Part 1: Average Rainfall\n")
+#print("Part 1: Average Rainfall\n")
 
-numYears = 0
+#numYears = 0
 
-while numYears <= 0:
-    numYears = int(input("Enter number of years (greater than zero): "))
+#while numYears <= 0:
+#    numYears = int(input("Enter number of years (greater than zero): "))
 
-totalRain = 0
+#totalRain = 0
 
-for count in range(numYears):
-    for month in range(1, 13):
-        if month == 1:
-            rainfall = int(input(f"Enter rainfall for January of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 2:
-            rainfall = int(input(f"Enter rainfall for February of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 3:
-            rainfall = int(input(f"Enter rainfall for March of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 4:
-            rainfall = int(input(f"Enter rainfall for April of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 5:
-            rainfall = int(input(f"Enter rainfall for May of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 6:
-            rainfall = int(input(f"Enter rainfall for June of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 7:
-            rainfall = int(input(f"Enter rainfall for July of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 8:
-            rainfall = int(input(f"Enter rainfall for August of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 9:
-            rainfall = int(input(f"Enter rainfall for September of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 10:
-            rainfall = int(input(f"Enter rainfall for October of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 11:
-            rainfall = int(input(f"Enter rainfall for November of year {count+1}: "))
-            totalRain += rainfall
-        elif month == 12:
-            rainfall = int(input(f"Enter rainfall for December of year {count+1}: "))
-            totalRain += rainfall
+#for count in range(numYears):
+#    for month in range(1, 13):
+#        if month == 1:
+#            rainfall = int(input(f"Enter rainfall for January of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 2:
+#            rainfall = int(input(f"Enter rainfall for February of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 3:
+#            rainfall = int(input(f"Enter rainfall for March of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 4:
+#            rainfall = int(input(f"Enter rainfall for April of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 5:
+#            rainfall = int(input(f"Enter rainfall for May of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 6:
+#            rainfall = int(input(f"Enter rainfall for June of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 7:
+#            rainfall = int(input(f"Enter rainfall for July of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 8:
+#            rainfall = int(input(f"Enter rainfall for August of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 9:
+#            rainfall = int(input(f"Enter rainfall for September of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 10:
+#            rainfall = int(input(f"Enter rainfall for October of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 11:
+#            rainfall = int(input(f"Enter rainfall for November of year {count+1}: "))
+#            totalRain += rainfall
+#        elif month == 12:
+#            rainfall = int(input(f"Enter rainfall for December of year {count+1}: "))
+#            totalRain += rainfall
 
-numMonths = numYears * 12
-averageRain = totalRain / numMonths
+#numMonths = numYears * 12
+#averageRain = round(totalRain / numMonths, 2)
 
-print(f"\nThe total rainfall across {numMonths} months was: {totalRain} inches")
-print(f"The average rainfall across {numMonths} months was: {averageRain} inches\n")
+#print(f"\nThe total rainfall across {numMonths} months was: {totalRain} inches")
+#print(f"The average rainfall across {numMonths} months was: {averageRain} inches\n")
 
 # Part 2: The CSU Global Bookstore has a book club that awards points to its students based on the number of books
 # purchased each month. The points are awarded as follows:

1ae5b4d6ca91388fc546885511af5ab7489df871
Brennan Derrick <bkderrick90@gmail.com>
2024-06-27 12:46:10 -0500

    Module 3 Completion

--- Module 7/module7.py
+++ Module 7/module7.py
@@ -33,3 +33,41 @@
 #
 # The program should let the user enter a course number and then it should display the course's room number,
 # instructor, and meeting time.
+
+def print_details(course_num):
+    print(f"\nDetails for {course_num.upper()}")
+    print("Room:", rooms[course_num.upper()])
+    print("Instructor:", instructors[course_num.upper()])
+    print("Meeting Time:", meeting_time[course_num.upper()])
+
+
+rooms = {"CSC101": 3004, "CSC102": 4501, "CSC103": 6755, "NET110": 1244, "COM241": 1411}
+instructors = {"CSC101": "Haynes", "CSC102": "Alvarado", "CSC103": "Rich", "NET110": "Burke", "COM241": "Lee"}
+meeting_time = {"CSC101": "8:00 am", "CSC102": "9:00 am", "CSC103": "10:00 am", "NET110": "11:00 am",
+                "COM241": "1:00 pm"}
+keep_searching = True
+
+while keep_searching:
+    print("Which course would you like to look up?")
+    for key in rooms.keys():
+        print(key)
+    course = input("Enter course number: ")
+
+    if course.upper() in rooms.keys():
+        print_details(course)
+    else:
+        print("\nThe course you entered is not in your list of courses. Would you like to add it?")
+        add_course = input("Y or N: ")
+
+        if add_course.upper() == "Y":
+            rooms[course.upper()] = input("Enter room number: ")
+            instructors[course.upper()] = input("Enter instructor: ")
+            meeting_time[course.upper()] = input("Enter meeting time: ")
+
+            print_details(course)
+
+    print("\nWould you like to look up another course?")
+    choice = input("Y or N: ")
+
+    if choice.upper() == "N":
+        keep_searching = False
